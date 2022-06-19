a16e1a22d84f7cf2aa017cc0aa4649b8973a2aae
Abigail Unwin <abigailunwin@gmail.com>
2022-06-19 19:15:50 +0100

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,21 @@
+import './js/scroll'
+
+import { handleSubmit} from './js/formHandler'
+import { updateUI } from './js/updateUI'
+import { postData } from './js/postData'
+import { scrollToTop } from './js/scroll'
+import { scrollFunction } from './js/scroll'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/results.scss'
+import './styles/header.scss'
+
+export { handleSubmit };
+export { updateUI };
+export { scrollToTop };
+export { scrollFunction };
+export { postData };
+
+
